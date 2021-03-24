b733c0085e7c944df8f000825e0e1bfd64a938b0
dipendradangol <dipendra.dangol11@gmail.com>
2021-03-24 15:33:41 +0200

    create additional pages and create Single Pages for CPTs

---
+++ wp-content/themes/lawfirm/single-practice-areas.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<?php while (have_posts()) : the_post(); ?>
+    <section class="elementor-section elementor-top-section elementor-element elementor-section-boxed content" data-element_type="section">
+		<div class="elementor-container elementor-column-gap-default">
+			<div class="elementor-column elementor-col-75 elementor-top-column elementor-element" data-element_type="column">
+			    <div class="elementor-widget-wrap">
+                    <h1><?php echo get_the_title(); ?></h1>
+                    <h2><?php echo do_shortcode('[acf field="blurb"]'); ?></h2>
+                    <?php echo the_content(); ?>
+                </div>
+		    </div>
+			<div class="elementor-column elementor-col-25 elementor-top-column elementor-element" data-element_type="column">
+                <div class="elementor-widget-wrap">
+                    <h3>All Practice Areas</h3>
+                    <?php echo do_shortcode('[practice_areas_link]'); ?>
+                </div>
+            </div>
+		</div>
+	</section>
+
+<?php endwhile; ?>
+<?php get_footer(); ?>
